59f98de0f558aa1d19724e50975469119810de00
ihsiag <iamanapple4@gmail.com>
2022-02-15 15:28:53 +0900

    d

--- of_v0.11.2_vs2017_release/apps/myApps/21_graduation_sketch-08/src/Scene_Animation.cpp
+++ of_v0.11.2_vs2017_release/apps/myApps/21_graduation_sketch-08/src/Scene_Animation.cpp
@@ -25,7 +25,7 @@ void Scene_Animation::resetScene() {
     ssGlobalLog << "CLEARED-ARRAYS" << endl;
 
     gk.importGK3D(gk.findLatestFilePath("./gk3dExported/", "gk3d"), meshToAnimate, gkPlanesCreatedFromMeshToAnimate, gkPlanesCreatedManuallyToAnimate);
-
+	initAnimationClasses();
 }
 
 void Scene_Animation::update() {
@@ -47,14 +47,17 @@ void Scene_Animation::draw() {
 
 	cam.begin();
 	glEnable(GL_DEPTH_TEST);
+	gk.draw3DPlaneGrid(10,50,glm::vec3(0,1,0),1,glm::vec4(0.6));
 	if (bPlayAnimation)animate();
+
+	for (auto& ac : animationClasses) { ac->draw(); }
 	drawMainMesh();
 	
 	glDisable(GL_DEPTH_TEST);
 	cam.end();
 
 	//-----------2D-LAYER-----------//
-
+	gk.drawGrid();
 	//-----------INFO-----------//
     createInfo(ssInstruct, ssProgramInfo, ssDebug);
 
@@ -69,6 +72,7 @@ void Scene_Animation::draw() {
 void Scene_Animation::initParam() {
 	bPlayAnimation = false;
 	animationFrame = 0;
+	animationIndex = 0;
 }
 
 void Scene_Animation::initSet() {
@@ -78,7 +82,7 @@ void Scene_Animation::initSliders() {
 }
 
 void Scene_Animation::resetCamera() {
-	cam.setDistance(14);
+	//cam.setDistance(14);
 	//cam.enableOrtho();
 	cam.setPosition(glm::vec3(180, 180, 180));
 	//cam.setVFlip(true);
@@ -89,6 +93,8 @@ void Scene_Animation::createInfo(stringstream& _ssInstruct, stringstream& _ssPro
     //-----------INFO-----------//--later put into update func.
     _ssInstruct << "INSTRUCTIONS: " << endl;
     _ssInstruct << "START / STOP             - S" << endl;
+	_ssInstruct << "RESET             - R" << endl;
+
 
 
 
@@ -100,43 +106,56 @@ void Scene_Animation::createInfo(stringstream& _ssInstruct, stringstream& _ssPro
     _ssProgramInfo << "CAMERA LOOK DIR: " << cam.getLookAtDir() << endl;
 }
 
+
+
 void Scene_Animation::drawMainMesh() {
-	glColor4f(0, 1, 0, 0.4);
-	meshToAnimate.drawWireframe();
 	glColor4f(0, 0, 1, 0.4);
 	meshToAnimate.drawFaces();
+	glLineWidth(1);
+	glColor4f(0, 1, 0, 0.4);
+	meshToAnimate.drawWireframe();
+	
 }
-
 
 void Scene_Animation::toggleAnimation() {
 	bPlayAnimation = !bPlayAnimation;
-	animationFrame = 0;
+}
+
+void Scene_Animation::initAnimationClasses() {
+	animationClasses.push_back(&amv);
+	animationClasses.push_back(&ame);
+	animationClasses.push_back(&amf);
+	animationClasses.push_back(&apf);
+	amv.setGPL(&gkPlanesCreatedFromMeshToAnimate);
+	ame.setGPL(&gkPlanesCreatedFromMeshToAnimate);
+	amf.setGPL(&gkPlanesCreatedFromMeshToAnimate);
+	apf.setGPL(&gkPlanesCreatedFromMeshToAnimate);
+	for (auto& ac : animationClasses) {
+		ac->setup();
+	}
+}
+
+void Scene_Animation::resetAnimationClasses() {
+	animationIndex = 0;
+	for (auto& ac : animationClasses) {
+		ac->resetAnimation();
+	}
 }
 
 void Scene_Animation::animate() {
-
-	int index = animationFrame/2;
-	if (index > gkPlanesCreatedFromMeshToAnimate.size()*3)index = gkPlanesCreatedFromMeshToAnimate.size()*3;
-	glPointSize(10);
-	glColor3f(1, 0, 0);
-	glBegin(GL_POINTS);
-	for (int i = 0; i < index; i++) {	
-		glm::vec3 _p = gkPlanesCreatedFromMeshToAnimate[i / 3].originalMesh.getVertex(i % 3);
-		glVertex3f(_p.x,_p.y,_p.z);
+	if (animationClasses[animationIndex]->getNextAnimationTriggerState())animationIndex++; //&& animationIndex < animationClasses.size() - 1
+	if (animationIndex == animationClasses.size())resetAnimationClasses();
+	for (int i = 0; i < animationIndex; i++) {
+		animationClasses[i]->update();
 	}
-	glEnd();
-	index = animationFrame / 2;
-	if (index > gkPlanesCreatedManuallyToAnimate.size())index = gkPlanesCreatedManuallyToAnimate.size();
-	for (int i = 0; i < index; i++) {
-		gkPlanesCreatedManuallyToAnimate[i].drawGKPlane();
-	}
+	animationClasses[animationIndex]->update();
+	rotateCamera();
 	animationFrame++;
 }
 
 void Scene_Animation::rotateCamera() {
-
-}
-
-void Scene_Animation::animateGKPCFM() {
-
+	float rotateR = sqrt(pow(140,2)*2);
+	float rotationSpeed = animationFrame * 0.005;
+	cam.setPosition(glm::vec3(rotateR * cos(PI/4+rotationSpeed), cam.getPosition().y, rotateR * sin(PI/4+rotationSpeed)));
+	cam.lookAt(ofVec3f(0, 0, 0), glm::vec3(0, 1, 0));
 }
--- of_v0.11.2_vs2017_release/apps/myApps/21_graduation_sketch-08/src/Scene_Animation.h
+++ of_v0.11.2_vs2017_release/apps/myApps/21_graduation_sketch-08/src/Scene_Animation.h
@@ -8,6 +8,284 @@
 #include "ofxGui.h"
 #include "ofEasyCam.h"
 
+class AnimationClass {
+public:
+	virtual void setup() = 0;
+	virtual void resetAnimation() =0;
+	virtual void update()=0;
+	virtual void draw()=0;
+	virtual bool getThisAnimationEndState()=0;
+	virtual bool getNextAnimationTriggerState() = 0;
+};
+
+class AnimationClassMeshVertex : public AnimationClass {
+public:
+	int animationFrame;
+	int planesNumToShow;
+	int dotsNumToShow;
+	bool bDrawAll;
+	bool bNextAnimationTrigger;
+	vector<GKPlane>* gpl;
+
+	AnimationClassMeshVertex(){}
+	
+
+	void setup() {
+		initParam();
+	}
+
+	void setGPL(vector<GKPlane>* _gpl) {
+		gpl = _gpl;
+	}
+
+	void initParam() {
+		animationFrame = 0;
+		planesNumToShow = 0;
+		dotsNumToShow = 0;
+		bDrawAll = false;
+		bNextAnimationTrigger = false;
+	}
+
+	void update() {
+		animationFrame++;
+		dotsNumToShow++;
+		if (dotsNumToShow > 2) {
+			dotsNumToShow = 0;
+			planesNumToShow++;
+		}
+		if (planesNumToShow > gpl->size() / 2)bNextAnimationTrigger = true;
+		if (planesNumToShow > gpl->size()) {
+			planesNumToShow = gpl->size();
+			dotsNumToShow = 3;
+			bDrawAll = true;
+		}
+	}
+
+	void draw() {
+		glPointSize(10);
+		glColor3f(1, 0, 0);
+		glBegin(GL_POINTS);
+		for (int i = 0; i < planesNumToShow; i++) {
+			if (i < planesNumToShow - 1) {
+				for (int j = 0; j < 3; j++) {
+					glm::vec3 _p = gpl->at(i).originalMesh.getVertex(j);
+					glVertex3f(_p.x, _p.y, _p.z);
+				}
+			}
+			else {
+				for (int j = 0; j < dotsNumToShow; j++) {
+					glm::vec3 _p = gpl->at(i).originalMesh.getVertex(j);
+					glVertex3f(_p.x, _p.y, _p.z);
+				}
+			}
+		}
+		glEnd();
+	}
+
+	bool getThisAnimationEndState() {
+		return bDrawAll;
+	}
+
+	bool getNextAnimationTriggerState() {
+		return bNextAnimationTrigger;
+	}
+
+	void resetAnimation() {
+		initParam();
+	}
+};
+
+class AnimationClassMeshEdge : public AnimationClass {
+public:
+	int animationFrame;
+	int planesNumToShow;
+	int dotsNumToShow;
+	bool bDrawAll;
+	bool bNextAnimationTrigger;
+	vector<GKPlane>* gpl;
+	
+	AnimationClassMeshEdge(){}
+
+	void setup() {
+		initParam();
+	}
+
+	void setGPL(vector<GKPlane>* _gpl) {
+		gpl = _gpl;
+	}
+
+	void initParam() {
+		animationFrame = 0;
+		planesNumToShow = 0;
+		dotsNumToShow = 0;
+		bDrawAll = false;
+		bNextAnimationTrigger = false;
+	}
+
+	void update() {
+		animationFrame++;
+		if(animationFrame%2 == 0)dotsNumToShow++;
+		if (dotsNumToShow > 4) {
+			dotsNumToShow = 0;
+			planesNumToShow++;
+		}
+		if (planesNumToShow > gpl->size() / 2) bNextAnimationTrigger = true;
+		if (planesNumToShow > gpl->size()) {
+			planesNumToShow = gpl->size();
+			dotsNumToShow = 4;
+			bDrawAll = true;
+		}
+	}
+
+	void draw() {
+		glLineWidth(2);
+		glColor3f(1, 0, 0);
+		for (int i = 0; i < planesNumToShow; i++) {
+			glBegin(GL_LINE_STRIP);
+			if (i < planesNumToShow - 1) {
+				for (int j = 0; j < 4; j++) {
+					glm::vec3 _p = gpl->at(i).originalMesh.getVertex(j%3);
+					glVertex3f(_p.x, _p.y, _p.z);
+				}
+			}
+			else {
+				for (int j = 0; j < dotsNumToShow; j++) {
+					glm::vec3 _p = gpl->at(i).originalMesh.getVertex(j%3);
+					glVertex3f(_p.x, _p.y, _p.z);
+				}
+			}
+			glEnd();
+		}
+	}
+
+	bool getNextAnimationTriggerState() {
+		return bNextAnimationTrigger;
+	}
+
+	bool getThisAnimationEndState() {
+		return bDrawAll;
+	}
+
+	void resetAnimation() {
+		initParam();
+	}
+
+};
+
+class AnimationClassMeshFace : public AnimationClass {
+public:
+	int animationFrame;
+	int planesNumToShow;
+	bool bDrawAll;
+	bool bNextAnimationTrigger;
+	vector<GKPlane>* gpl;
+
+	AnimationClassMeshFace() {}
+
+	void setup() {
+		initParam();
+	}
+
+	void setGPL(vector<GKPlane>* _gpl) {
+		gpl = _gpl;
+	}
+
+	void initParam() {
+		animationFrame = 0;
+		planesNumToShow = 0;
+		bDrawAll = false;
+		bNextAnimationTrigger = false;
+	}
+
+	void update() {
+		animationFrame++;
+		planesNumToShow++;
+		if (planesNumToShow > gpl->size())bNextAnimationTrigger = true;
+		if (planesNumToShow > gpl->size()) {
+			planesNumToShow = gpl->size();
+			bDrawAll = true;
+		}
+	}
+
+	void draw() {
+		glLineWidth(2);
+		glColor3f(1, 0, 0);
+		for (int i = 0; i < planesNumToShow; i++) {
+			gpl->at(i).drawInputMesh();
+		}	
+	}
+
+	bool getNextAnimationTriggerState() {
+		return bNextAnimationTrigger;
+	}
+
+	bool getThisAnimationEndState() {
+		return bDrawAll;
+	}
+
+	void resetAnimation() {
+		initParam();
+	}
+
+};
+
+class AnimationClassPlaneFace : public AnimationClass {
+public:
+	int animationFrame;
+	int planesNumToShow;
+	bool bDrawAll;
+	bool bNextAnimationTrigger;
+	vector<GKPlane>* gpl;
+
+	AnimationClassPlaneFace() {}
+
+	void setup() {
+		initParam();
+	}
+
+	void setGPL(vector<GKPlane>* _gpl) {
+		gpl = _gpl;
+	}
+
+	void initParam() {
+		animationFrame = 0;
+		planesNumToShow = 0;
+		bDrawAll = false;
+		bNextAnimationTrigger = false;
+	}
+
+	void update() {
+		animationFrame++;
+		planesNumToShow++;
+		if (planesNumToShow > gpl->size())bNextAnimationTrigger = true;
+		if (planesNumToShow > gpl->size()) {
+			planesNumToShow = gpl->size();
+			bDrawAll = true;
+		}
+	}
+
+	void draw() {
+		glLineWidth(2);
+		glColor3f(1, 0, 0);
+		for (int i = 0; i < planesNumToShow; i++) {
+			gpl->at(i).drawGKPlane();
+		}
+	}
+
+	bool getNextAnimationTriggerState() {
+		return bNextAnimationTrigger;
+	}
+
+	bool getThisAnimationEndState() {
+		return bDrawAll;
+	}
+
+	void resetAnimation() {
+		initParam();
+	}
+
+};
+
 class Scene_Animation : public GKScene {
 public:
 	Scene_Animation() {}
@@ -26,6 +304,13 @@ public:
 	bool bPlayAnimation;
 	unsigned long int animationFrame;
 
+	AnimationClassMeshVertex amv;
+	AnimationClassMeshEdge ame;
+	AnimationClassMeshFace amf;
+	AnimationClassPlaneFace apf;
+	vector<AnimationClass*> animationClasses;
+	int animationIndex;
+
 	void setup();
 	void resetScene();
 	void update();
@@ -36,6 +321,8 @@ public:
 	void initSet();
 	void initSliders();
 	void resetCamera();
+	void initAnimationClasses();
+	void resetAnimationClasses();
 
 	void createInfo(stringstream& _ssInstruct, stringstream& _ssProgramInfo, stringstream& _ssDebug);
 
@@ -44,11 +331,12 @@ public:
 	void toggleAnimation();
 	void animate();
 	void rotateCamera();
+
 	void animateGKPCFM();
 	void animateGKPCM();
-	void animateVertex();
-	void animateEdge();
-	void animateFace();
+	void animateMeshVertex(const float& _animationSpeed, vector<GKPlane>& _gpl);
+	void animateMeshEdge(const float& _animationSpeed, vector<GKPlane>& _gpl);
+	void animateMeshFace(const float& _animationSpeed, vector<GKPlane>& _gpl);
 	void animateIntersection();
 	void animateGeneration();
 	void animateFilling();
@@ -64,6 +352,10 @@ public:
 		case 's':
 			toggleAnimation();
 			ssGlobalLog << "PLAY" << endl;
+			break;
+		case 'r':
+			resetAnimationClasses();
+			ssGlobalLog << "RESET" << endl;
 			break;
 		}
 	}
